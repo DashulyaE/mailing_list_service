270c73bdc6e3e0ac50896b75301a2f803d802ddc
Darya <dasha-439@yandex.ru>
2025-08-10 13:34:49 +0300

    Создан класс Миксин для стилизации формы

--- mailingserv/forms.py
+++ mailingserv/forms.py
@@ -1,21 +1,29 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 
 from mailingserv.models import Mailing, Client, Message
 
+class StyleFormMixin():
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = 'form-check-input'
+            else:
+                fild.widget.attrs['class'] = 'form-control'
 
-class MailingForm(ModelForm):
+class MailingForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Mailing
         fields = "__all__"
 
 
-class ClientForm(ModelForm):
+class ClientForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Client
         fields = "__all__"
 
 
-class MessageForm(ModelForm):
+class MessageForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Message
         fields = "__all__"
